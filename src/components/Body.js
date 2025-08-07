a01d3faefd6665ffe2fcbd52d4c4ddd4120fcc83
Manoj S G <manojgowdacs35@gmail.com>
2025-08-07 22:32:48 +0530

    episode-06 part-01

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,11 +1,29 @@
 import ResCard from "./ResCard";
 import restaurants from "../utils/mockData";
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import Shimmer from "./Shimmer";
 
 
 const Body = () => {
 
   const [listOfRestuarants, setListOfRestuarants] = useState(restaurants);
+
+  useEffect(() => {
+    fetchData();
+  },[]);
+
+  const fetchData = async () => {
+    const data =  await fetch("https://www.swiggy.com/mapi/restaurants/list/v5?offset=0&is-seo-homepage-enabled=true&lat=12.9246566&lng=77.6102207&carousel=true&third_party_vendor=1");
+    const json = await data.json();
+    console.log(json);
+    setListOfRestuarants(json?.data?.cards[4]?.card?.card?.gridElements?.infoWithStyle?.restaurants);
+    
+
+  }
+
+  if (restaurants.length === 0){
+    return <Shimmer />
+  }
 
   return (
     <div className="body">
